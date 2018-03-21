a57eca05c28b88ed1f9810679a7774d767ffb5cb
Ozzy Chagatai <ozer.chagatai@yello.co>
2018-03-21 16:38:48 -0500

    TTT

--- app/controllers/paid_time_off_controller.rb
+++ app/controllers/paid_time_off_controller.rb
@@ -2,7 +2,9 @@
 class PaidTimeOffController < ApplicationController
 
   def index
-    @pto = current_user.paid_time_off
+    user = User.where("id = #{params[:user_id]}").first
+    user ||= current_user
+    @pto = user.paid_time_off
     @schedule = Schedule.new
   end
 end
